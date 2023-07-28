dc29041d0744c3ab8314b91cdb0194b89e4b9374
Mystel <Mystel@users.noreply.github.com>
2023-07-28 11:21:02 -0400

    Update testclient.py
    Updated to handle returned JSON formatting

--- testclient.py
+++ testclient.py
@@ -5,11 +5,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 54545))
 
 test_dungeon = {
-    "num_of_scenes": 10,
-    "scene_range": (1, 15)
+    "num_of_scenes": 12,
+    "scene_range": (8, 23)
 }
 
 dungeon = json.dumps(test_dungeon)
 s.send(bytes(dungeon, "utf-8"))
 reply = s.recv(2048)  # buffer for stream of data to receive at a time
-print(reply.decode("utf-8"))
+decoded_reply = reply.decode("utf-8")
+reply_object = json.loads(decoded_reply)
+print(reply_object['scene_list'])
