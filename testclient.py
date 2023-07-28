3555bfe5207615d2aee338eb28707e93e15542a7
Mystel <Mystel@users.noreply.github.com>
2023-07-28 10:34:34 -0400

    Update testclient.py

--- testclient.py
+++ testclient.py
@@ -5,7 +5,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 54545))
 
 test_dungeon = {
-    "difficulty": "easy",
     "num_of_scenes": 10,
     "scene_range": (1, 15)
 }
